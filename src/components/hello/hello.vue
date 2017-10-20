a303886b743f0509adca9a8f428a7305af99d226
Scott Wade <sc0ttwad3@gmail.com>
2017-10-20 12:50:47 -0400

    Adjust font sizes and weights in Hello component

--- src/components/hello/hello.vue
+++ src/components/hello/hello.vue
@@ -3,12 +3,8 @@
     <h1>{{ msg }}</h1>
     <h2>{{ "hello-essential-links" | format-message }}</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li><a href="https://vuejs.org/v2/guide/" target="_blank">docs</a></li>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">template-docs</a></li>
     </ul>
     <h2>{{ "hello-ecosystems" | format-message }}</h2>
     <ul>
